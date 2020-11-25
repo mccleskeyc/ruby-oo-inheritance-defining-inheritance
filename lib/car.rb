3e298c7f2ea852575f9b494f3d407dd710e0bad9
mccleskeyc <mccleskeyc@gmail.com>
2020-11-25 10:15:13 -0500

    commit tree 9751239704ee85bc9611c1a2818dfed7a04e7d23

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,11 @@
 require_relative "./vehicle.rb"
+class Car < Vehicle # we use < to inherit the Car class from Vehicle
+# even though there is nothing in our Car class when we ran the above, it contains all of the methods it inherited from Vehicle
+
+def go # the test was expecting a different output here, so we overwrite our go method inherited from vehicle by including it as we need it to run here.
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+end
+end
+
+## How is it executed?
+# When the program is being executed, at the point at which the go method is involed, the compiler will first look in the class to which the instance of car that we are calling the mehod on belongs. If it finds a go method there it will execute it, otherwise it will move on to look in the parent class that this inherits from.
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,16 @@
 class Vehicle
+    attr_accessor :wheel_size, :wheel_number
 
+    def initialize(wheel_size, wheel_number) #instances of Vehicle initialize with a wheel size and number
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
+
+    def go
+        "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank
+        "filling up!"
+    end
 end
